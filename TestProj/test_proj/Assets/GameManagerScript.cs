c562ad946df15f9344ef178192bd4e4a0d089e1c
Jason Ehrlich <fnoogi3@gmail.com>
2021-09-13 09:22:53 -0400

    Prefab Variants, Better UI

--- TestProj/test_proj/Assets/GameManagerScript.cs
+++ TestProj/test_proj/Assets/GameManagerScript.cs
@@ -5,7 +5,8 @@ using TMPro;
 
 public class GameManagerScript : MonoBehaviour
 {
-    public GameObject stickPreFab, stonePreFab, player, shopUI;
+    public GameObject[] stickPreFab = new GameObject[3], stonePreFab = new GameObject[2];
+    public GameObject player, shopUI;
     
     public Vector2 SpawnBoundsX, SpawnBoundsZ;
     [HideInInspector]
@@ -21,18 +22,18 @@ public class GameManagerScript : MonoBehaviour
     public int stickPickupIncreasePurchaseAmount = 0, pickupMoreStickCost = 25;
     //stone upgrades
     [HideInInspector]
-    public bool stonesUnlocked = false;
+    public bool stonesUnlocked = true;
     [HideInInspector]
     public int stonePickupIncreasePurchaseAmount = 0, unlockStonesCost = 10, pickupMoreStoneCost = 25;
 
 
     [HideInInspector]
     public float timeNow = 0f, timeMaxDefault = 20f, timeIncreaseUpgrade = 10f;
-    [HideInInspector]
+    //[HideInInspector]
     public int 
         sticks = 0, spendingSticks = 50,
         stones = 0, spendingStones = 50;
-    public TextMeshProUGUI stickText, stoneText, time;
+    public TextMeshProUGUI stickText, shopStickText, stoneText, shopStoneText, time;
     // Start is called before the first frame update
     void Start()
     {
@@ -65,27 +66,31 @@ public class GameManagerScript : MonoBehaviour
             time.text = "Time Left: " + ((timeMaxDefault + (timeIncreaseUpgrade * timeIncreasePurchaseAmount)) - timeNow).ToString("F2");
         }
         shopUI.SetActive(shopping);
+        stickText.gameObject.SetActive(!shopping);
+        stoneText.gameObject.SetActive(!shopping);
+        shopStickText.gameObject.SetActive(shopping);
+        shopStoneText.gameObject.SetActive(shopping);
     }
 
     public void newSpawn()
     {
+        int prefabNum = Random.Range(0, 2);
         if (stonesUnlocked)
         {
             float rng = Random.value;
             if (rng >= 0.5f)
-                Instantiate(stickPreFab, new Vector3(Random.Range(SpawnBoundsX.x, SpawnBoundsX.y), 0f, Random.Range(SpawnBoundsZ.x, SpawnBoundsZ.y)), Quaternion.Euler(90, 0, Random.Range(0, 360)));
+            {
+                Instantiate(stickPreFab[prefabNum], new Vector3(Random.Range(SpawnBoundsX.x, SpawnBoundsX.y), 0f, Random.Range(SpawnBoundsZ.x, SpawnBoundsZ.y)), Quaternion.Euler(90, 0, Random.Range(0, 360)));
+            }
             else
             {
-                GameObject obj = Instantiate(stonePreFab, new Vector3(Random.Range(SpawnBoundsX.x, SpawnBoundsX.y), 0f, Random.Range(SpawnBoundsZ.x, SpawnBoundsZ.y)), Quaternion.Euler(90, 0, Random.Range(0, 360)));
-                float scale = Random.Range(0.1f, 0.7f);
-                obj.transform.localScale = new Vector3(scale, scale, scale);
-                obj.GetComponent<SphereCollider>().radius = scale;
+                GameObject obj = Instantiate(stonePreFab[prefabNum], new Vector3(Random.Range(SpawnBoundsX.x, SpawnBoundsX.y), 0f, Random.Range(SpawnBoundsZ.x, SpawnBoundsZ.y)), Quaternion.Euler(90, 0, Random.Range(0, 360)));
             }
 
         }
         else
         {
-            Instantiate(stickPreFab, new Vector3(Random.Range(SpawnBoundsX.x, SpawnBoundsX.y), 0f, Random.Range(SpawnBoundsZ.x, SpawnBoundsZ.y)), Quaternion.Euler(90, 0, Random.Range(0, 360)));
+            Instantiate(stickPreFab[prefabNum], new Vector3(Random.Range(SpawnBoundsX.x, SpawnBoundsX.y), 0f, Random.Range(SpawnBoundsZ.x, SpawnBoundsZ.y)), Quaternion.Euler(90, 0, Random.Range(0, 360)));
         }
     }
 
@@ -102,7 +107,7 @@ public class GameManagerScript : MonoBehaviour
 
     public void updateSticksAndStones()
     {
-        stickText.text = "Spending Sticks: " + spendingSticks;
-        stoneText.text = "Spending Stones: " + spendingStones;
+        shopStickText.text = "Spending Sticks: " + spendingSticks;
+        shopStoneText.text = "Spending Stones: " + spendingStones;
     }
 }
